9b07b077e0facf75d727adc4834a1e58907db575
Alexander Korolev <alexander.korolev@h-d-gmbh.de>
2024-06-25 14:23:47 +0200

    publish simple cmake and clang features

---
+++ src/cmake/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+
+apt_get_update
+
+check_packages cmake
