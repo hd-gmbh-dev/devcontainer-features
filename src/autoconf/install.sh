64958bfa44fe5b62f563ca6e9e5bb852661fddf9
Alexander Korolev <alexander.korolev@h-d-gmbh.de>
2024-06-25 15:33:18 +0200

    try to leave only build-essential

--- src/autoconf/install.sh
+++ src/autoconf/install.sh
@@ -30,4 +30,5 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt_get_update
 
-check_packages autoconf autotools-dev automake build-essential
+check_packages build-essential
+# check_packages autoconf autotools-dev automake build-essential
